c0f56c8de99125ab9e67d9e2c5138280fd27fff6
huypham <huy9008437@gmail.com>
2025-02-19 21:13:51 +0700

    render data ra giao diện

--- controller/client/products/product.controller.js
+++ controller/client/products/product.controller.js
@@ -1,3 +1,22 @@
-module.exports.index = (req, res) => {
-  res.render('client/pages/products/product.pug', { pageTitle: 'Products' })
+const Product = require('../../../models/product.model.js')
+
+
+
+// khi cái route chạy thì nó yêu cầu controller lấy data từ db ra 
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: 'active',
+    deleted: false
+  })
+
+  const newProducts = products.map(item => {
+    item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed()
+    return item;
+  })
+
+  // các biến được render sẽ có thể sử dụng trong file pug bên dưới
+  res.render('client/pages/products/product.pug', {
+    pageTitle: 'Products',
+    products: newProducts
+  })
 }
--- models/product.model.js
+++ models/product.model.js
@@ -11,7 +11,6 @@ const productSchema = new mongoose.Schema(
     status: String,
     position: Number,
     deleted: Boolean
-
   }
 
 )
