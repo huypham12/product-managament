966c95d1a892c902cc7816572d616157036ae4e6
huypham <huy9008437@gmail.com>
2025-02-20 13:17:21 +0700

    render data

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,3 +1,24 @@
-module.exports.product = (req, res) => {
-  res.render('admin/pages/products/index.pug')
+
+// lấy data
+const Product = require('../../models/product.model.js')
+
+
+
+// GET /admin/products
+module.exports.product = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  })
+
+  const newProducts = products.map(item => {
+    item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed()
+    return item;
+  })
+
+  res.render('admin/pages/products/index.pug',
+    {
+      pageTitle: 'Products',
+      products: newProducts
+    }
+  )
 }
